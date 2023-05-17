36e1d6d05d19194c26d80d866df4ee099e58ebff
sidedevelopers <56724326+sidedevelopers@users.noreply.github.com>
2023-05-17 12:49:32 +0000

    updated

--- footer.php
+++
@@ -1,8 +0,0 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-<script src="assets/OwlCarousel/dist/owl.carousel.js"></script>
-<script src="assets/main-js/main.js"></script>
-
-</body>
-
-</html>
