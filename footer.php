b951f96a4d3b5e7e0b4d73f9233d63f1441328e5
sidedevelopers <amanagarwal92594@gmail.com>
2023-05-17 17:40:54 +0530

    updated

---
+++ footer.php
@@ -0,0 +1,8 @@
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
+<script src="assets/OwlCarousel/dist/owl.carousel.js"></script>
+<script src="assets/main-js/main.js"></script>
+
+</body>
+
+</html>
